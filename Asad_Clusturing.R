4836170ed9d26daa6cee090e6d5184f0bf00512c
Muhammad Asad Khan <asad.shani7@gmail.com>
2017-07-09 18:53:03 +0200

    code , data sets
    Code is in Asad_Clustering.R file and further googleUSA5_1.csv , googleUSA5_2.csv is my data sets where i apply clustering technique to analyze the data for specific key words search in the world and in USA states.

---
+++ Asad_Clusturing.R
@@ -0,0 +1,38 @@
+
+# Cluster Analysis
+
+
+# Load Data ##########################
+Asad_C1 <- read.csv("googleUSA5_1.csv", header = TRUE)
+Asad_C2 <- read.csv("googleUSA5_2.csv", header = TRUE)
+colnames(Asad_C1)
+colnames(Asad_C2)
+
+# Use numerical data only
+clC1 <- Asad_C1[, 2:4]
+row.names(Asad_C1) <- clusterC1[, 1]
+colnames(clC1)
+
+clC2 <- Asad_C2[, 2:6]
+row.names(clC2) <- Asad_C2[, 1]
+colnames(clC2)
+
+?hclust
+
+# Clustering #############################
+
+# Create distance matrix
+dm1 <- dist(clC1)
+
+# Hierarchical clustering
+c1 <- hclust(dm1)
+c1    # Show info on clustering
+
+# Plot dendrogram of clusters
+plot(c1, main = "Cluster for searches unicornData virtualization, Internet security")
+
+
+plot(hclust(dist(clC2)), main = "Cluster for searches unicorn, Internet Security, Data mining, brexit, fifa")
+
+
+
